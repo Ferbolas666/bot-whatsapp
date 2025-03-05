6e908be14e17436e0e5752aa89b0b54ef28de64f
Ferbolas666 <maatheuss.celestinoo@gmail.com>
2025-03-05 15:34:10 -0300

    Alterando a porta para 3000 (ou outra porta disponível)

--- bot.js
+++ bot.js
@@ -223,7 +223,7 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(1050, '0.0.0.0', () => {
-    console.log("🌍 Servidor rodando em http://0.0.0.0:1050");
+server.listen(3000, '0.0.0.0', () => {
+    console.log("🌍 Servidor rodando em http://0.0.0.0:3000");
 });
 
